713fae9ceab88cc0810cd399d9d671a312025420
anan <dingwuying@totanota.com>
2025-05-19 17:53:46 +0300

    Add ConfigModule to support .env file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+const imports = [ConfigModule.forRoot(), UsersModule, AuthModule];
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: imports,
   controllers: [AppController],
   providers: [AppService],
 })
