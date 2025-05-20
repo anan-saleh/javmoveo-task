bcf56cc4219e66b04328e953c31e7d4325ecf576
anan <dingwuying@totanota.com>
2025-05-20 14:57:23 +0300

    Add tailwind style to register and login form

--- app/web/src/components/Login/index.tsx
+++ app/web/src/components/Login/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { useAuth } from '../../context/useAuth';
 import { useNavigate } from 'react-router-dom';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
 
 export const Login: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const { login } = useAuth();
   const navigate = useNavigate();
   const handleSubmit = async (e: React.FormEvent) => {
@@ -19,35 +20,67 @@ export const Login: React.FC = () => {
       } else {
         navigate('/player/dashboard');
       }
-      setMessage('Logged in successfully');
     } catch (error) {
-      setMessage('Login failed');
       console.error(error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username: </label>
-        <input
-          type="text"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Password: </label>
-        <input
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Login</button>
-      <p>{message}</p>
-    </form>
+    <div className="min-h-screen flex items-center justify-center px-4 bg-[#fefbf7]">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-sm bg-white p-6 rounded-lg space-y-5 shadow-md"
+      >
+        <div className="text-center">
+          <h2 className="text-gray-600 text-lg">Welcome to JaMoveo</h2>
+          <h1 className="text-3xl font-bold text-yellow-800">Register</h1>
+        </div>
+
+        <div>
+          <label className="block text-sm font-medium mb-1">Username*</label>
+          <input
+            type="text"
+            placeholder="Select your username"
+            className="w-full px-4 py-2 bg-[#f3eee4] rounded-md focus:outline-none"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium mb-1">Create password*</label>
+          <div className="relative">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder="Your Password"
+              className="w-full px-4 py-2 bg-[#f3eee4] rounded-md pr-10 focus:outline-none"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button
+              type="button"
+              className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
+              onClick={() => setShowPassword((prev) => !prev)}
+            >
+              {showPassword ? <FiEyeOff size={20} /> : <FiEye size={20} />}
+            </button>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-yellow-400 hover:bg-yellow-500 transition-colors py-2 rounded-md font-semibold text-black"
+        >
+          Register
+        </button>
+
+        <p className="text-center text-sm text-gray-500">
+          Don’t have an account?{' '}
+          <a href="/register" className="text-yellow-800 font-semibold">
+            Register
+          </a>
+        </p>
+      </form>
+    </div>
   );
 };
--- app/web/src/components/Signup/index.tsx
+++ app/web/src/components/Signup/index.tsx
@@ -1,60 +1,117 @@
 import React, { useState } from 'react';
+import { useAuth } from '../../context/useAuth';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
+
+const instruments = ['Guitar', 'Piano', 'Drums', 'Violin', 'Bass'];
 
 export const Signup: React.FC = () => {
+  const { register } = useAuth();
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const [instrument, setInstrument] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
-  const [message, setMessage] = useState('');
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    setMessage('account created successfully');
+    try {
+      await register({
+        username, password, instrument, isAdmin
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Username: </label>
-        <input
-          type="text"
-          value={username}
-          onChange={e => setUsername(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Password: </label>
-        <input
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      {!isAdmin && (
+    <div className="min-h-screen flex items-center justify-center px-4 bg-[#fefbf7]">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-sm bg-white p-6 rounded-lg space-y-5 shadow-md"
+      >
+        <div className="text-center">
+          <h2 className="text-gray-600 text-lg">Welcome to JaMoveo</h2>
+          <h1 className="text-3xl font-bold text-yellow-800">Register</h1>
+        </div>
+
         <div>
-          <label>Instrument: </label>
+          <label className="block text-sm font-medium mb-1">Username*</label>
           <input
             type="text"
-            value={instrument}
-            onChange={e => setInstrument(e.target.value)}
+            placeholder="Select your username"
+            className="w-full px-4 py-2 bg-[#f3eee4] rounded-md focus:outline-none"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             required
           />
         </div>
-      )}
-      <div>
-        <label>
+
+        <div>
+          <label className="block text-sm font-medium mb-1">Your instrument*</label>
+          <select
+            className="w-full px-4 py-2 bg-[#f3eee4] rounded-md focus:outline-none"
+            value={instrument}
+            onChange={(e) => setInstrument(e.target.value)}
+            required
+          >
+            <option value="">Select your instrument</option>
+            {instruments.map((inst) => (
+              <option key={inst} value={inst}>
+                {inst}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label className="block text-sm font-medium mb-1">Create password*</label>
+          <div className="relative">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder="Your Password"
+              className="w-full px-4 py-2 bg-[#f3eee4] rounded-md pr-10 focus:outline-none"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button
+              type="button"
+              className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500"
+              onClick={() => setShowPassword((prev) => !prev)}
+            >
+              {showPassword ? <FiEye size={20} /> : <FiEyeOff size={20} /> }
+            </button>
+          </div>
+        </div>
+
+
+        <div className="flex items-center space-x-2">
           <input
             type="checkbox"
+            id="isAdmin"
             checked={isAdmin}
-            onChange={e => setIsAdmin(e.target.checked)}
-          />{' '}
-          Register as Admin
-        </label>
-      </div>
-      <button type="submit">Sign Up</button>
-      <p>{message}</p>
-    </form>
+            onChange={(e) => setIsAdmin(e.target.checked)}
+            className="accent-yellow-600"
+          />
+          <label htmlFor="isAdmin" className="text-sm">
+            Is user an admin?
+          </label>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-yellow-400 hover:bg-yellow-500 transition-colors py-2 rounded-md font-semibold text-black"
+        >
+          Register
+        </button>
+
+        <p className="text-center text-sm text-gray-500">
+          Already have an account?{' '}
+          <a href="/login" className="text-yellow-800 font-semibold">
+            Log In
+          </a>
+        </p>
+      </form>
+    </div>
   );
 };
