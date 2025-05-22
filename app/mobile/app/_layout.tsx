ea77cb9badfd298812a66d2c39da14bdefc228fa
anan <dingwuying@totanota.com>
2025-05-22 06:15:09 +0300

    Add global css and disable horiozntal scroll

--- app/mobile/app/_layout.tsx
+++ app/mobile/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { AuthProvider } from "@/components/context/useAuth";
 import { Slot } from 'expo-router';
+import '../assets/css/global.css';
 
 export default function RootLayout() {
   return (
