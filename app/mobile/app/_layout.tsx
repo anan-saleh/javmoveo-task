ba1d863b3b953b4d68023ac39124a607d75efa54
anan <dingwuying@totanota.com>
2025-05-22 13:27:58 +0300

    Add FlashMessage component globally

--- app/mobile/app/_layout.tsx
+++ app/mobile/app/_layout.tsx
@@ -1,11 +1,13 @@
 import { AuthProvider } from "@/components/context/useAuth";
 import { Slot } from 'expo-router';
+import FlashMessage from 'react-native-flash-message';
 import '../assets/css/global.css';
 
 export default function RootLayout() {
   return (
     <AuthProvider>
       <Slot />
+      <FlashMessage position="top" />
     </AuthProvider>
   );
 }
