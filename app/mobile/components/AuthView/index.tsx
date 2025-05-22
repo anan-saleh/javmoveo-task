add30e27bbdd2bc37cf7c186d1839696a3a8a50f
anan <dingwuying@totanota.com>
2025-05-22 10:16:42 +0300

    Fix user logged in routing to auth page

--- app/mobile/components/AuthView/index.tsx
+++ app/mobile/components/AuthView/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet, Image, Platform, useWindowDimensions } from 'react-native';
 import AuthForm from './AuthForm';
 import { useRoute } from '@react-navigation/native';
+import { useAuth } from '../context/useAuth';
+import { useRouter } from 'expo-router';
 
 export const LoginView: React.FC = () => {
   const route = useRoute();
+  const router = useRouter();
   const isRegister = route.name === 'register';
   const source = isRegister ? require('../../assets/images/register.png') : require('../../assets/images/login.png');
   const { width } = useWindowDimensions();
+  const { user } = useAuth();
+  useEffect(() => {
+    if (user) {
+      router.navigate(user.isAdmin ? '/admin' : '/player');
+    }
+  }, [user]); 
   const isWideScreen = Platform.OS === 'web' && width >= 768;
   if (isWideScreen) {
     return (
